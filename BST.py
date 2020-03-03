9ab83357221b993adc525e5709e128e1abfbbb01
justinschuster <schujustin@gmail.com>
2020-03-03 13:14:27 -0500

    better file structure

---
+++ BST.py
@@ -0,0 +1,13 @@
+import Node
+
+class BST:
+	def __init__(self) -> None:
+		self.root = Node.Node()
+
+	# inserts Node into BST
+	def insertIter(self, node: Node) -> None:
+		return
+
+	# deletes Node from BST
+	def deleteIter():
+		return
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#	Justin Schuster
+#	CS435-004
+#	Project 1
+#	BST methods implementation
+
+import BST
+
 def main():
+	print("main")
 
 if __name__ == "__main__":
 	main()
