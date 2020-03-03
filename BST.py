27d747a6cfb02a038584da62de0f2206384ac500
justinschuster <schujustin@gmail.com>
2020-03-03 13:26:18 -0500

    Added function definitons

--- BST.py
+++ BST.py
@@ -5,9 +5,28 @@ class BST:
 		self.root = Node.Node()
 
 	# inserts Node into BST
-	def insertIter(self, node: Node) -> None:
+	def insertIter(self, root: Node) -> None:
+		# if node is null
+			# return new data node 
+		# Set (L|R) child = insertIter(valu)
 		return
 
 	# deletes Node from BST
 	def deleteIter():
 		return
+
+	# finds next Node in BST 
+	def findNextIter():
+		return
+
+	# finds the previous Node in BST
+	def findPrevIter():
+		return
+
+	# finds the minimum Node value in the BST
+	def findMinIter():
+		return 
+		
+	# finds the maximum Node value in the BST
+	def findMaxIter():
+		return 
