e5be19bc7919122a3af108deb590fcf387feec68
justinschuster <schujustin@gmail.com>
2020-03-08 15:49:36 -0400

    Added parent variable to Node class

--- Part2/Node.py
+++ Part2/Node.py
@@ -1,6 +1,7 @@
 class Node:
-	def __init__(self, key = None, height = 0):
+	def __init__(self, key = None, height = 0, parent = None):
 		self.key = key
 		self.height = height 
+		self.parent = parent
 		self.left = None
 		self.right = None 
